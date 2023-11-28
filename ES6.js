7f8ace25d70259d6bd762f93464ca6521eea5505
rabbaniinamdar <mahammedrabbani822931@gmail.com>
2023-11-28 09:58:07 +0530

    [Rabbani]:learned ES6 and ES7 features

--- ES6.js
+++ ES6.js
@@ -1,41 +1,67 @@
-// let and const Declarations:
-let n = 10;
-const pi = 3.14;
+// // let and const Declarations:
+// let n = 10;
+// const pi = 3.14;
 
-// Regular function
-function add(a, b) {
-    return a + b;
+// // Regular function
+// function add(a, b) {
+//     return a + b;
+// }
+// // Arrow function
+// const add = (a, b) => a + b;
+
+// //   Template Literals:
+// let name = "John";
+// console.log(`Hello, ${name}!`);
+
+// // Array destructuring
+// const [x, y] = [1, 2];
+
+// // Object destructuring
+// obj = {
+//     firstname: "John",
+//     age: 30
+// };
+// const { firstname, age } = obj
+
+// // Spread operator
+// const arr1 = [1, 2, 3];
+// const arr2 = [...arr1, 4, 5];
+
+// // Rest operator
+// const { a, b, ...rest } = { a: 1, b: 2, c: 3, d: 4 };
+
+function example(height = 50, width = 40) { 
+    const newH = height * 10;
+    const newW = width * 10;
+    return newH + newW;
 }
-// Arrow function
-const add = (a, b) => a + b;
 
-//   Template Literals:
-let name = "John";
-console.log(`Hello, ${name}!`);
+example(); // 900 (50*10 + 40*10)
 
-// Array destructuring
-const [x, y] = [1, 2];
+//class
+class Animal {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+    // This is a property on the prototype chain
+    toString() {
+        console.log('name:' + this.name + ', color:' + this.color);
+    }
+}
+class Cat extends Animal {
+    constructor(action) {
+        super('cat', 'white');
+        this.action = action;
+    }
+    toString() {
+        super.toString();
+        console.log(this.action)
+    }
+}
 
-// Object destructuring
-obj = {
-    firstname: "John",
-    age: 30
-};
-const { firstname, age } = obj
+var animal = new Animal('myDog', 'yellow'); // instantiate
+animal.toString(); // name: myDog, color: yellow
 
-// Spread operator
-const arr1 = [1, 2, 3];
-const arr2 = [...arr1, 4, 5];
-
-// Rest operator
-const { a, b, ...rest } = { a: 1, b: 2, c: 3, d: 4 };
-
-
-// ---------------------------------------------------------
-
-const numbers = [1, 2, 3, 4, 5];
-console.log(numbers.includes(3)); // true
-console.log(numbers.includes(6)); // false
-
-let result = 2 ** 3; // 2 to the power of 3 (2 * 2 * 2)
-console.log(result); // 8
+var cat = new Cat('catch')
+cat.toString();
---
+++ ES7.js
@@ -0,0 +1,11 @@
+
+// ---------------------------------------------------------
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers.includes(3)); // true
+console.log(numbers.includes(6)); // false
+
+let result = 2 ** 3; // 2 to the power of 3 (2 * 2 * 2)
+console.log(result); // 8
+console.log(Math.pow(2, 10)); // 1024
+
